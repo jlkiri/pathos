941f2a075ad6e8e6079e310bd3186bab59f56918
jlkiri <mail@kirillvasiltsov.com>
2024-03-09 01:01:21 +0900

    wefwef

--- path0/path0.s
+++ path0/path0.s
@@ -2,8 +2,8 @@
     .section .text
 
 _start:
-    la       sp, stack_top
+    la       sp, 0xc00000000
     call     main
-    mv       a0, a0        # Copy return value as is to a syscall argument register
-    li       a7, 3         # Call exit syscall
+    mv       a0, a0          # Copy return value as is to a syscall argument register
+    li       a7, 3           # Call exit syscall
     ecall
