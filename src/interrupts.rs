683e119c2e6f8541ef7dbbb57c677154e3390ed9
jlkiri <mail@kirillvasiltsov.com>
2024-03-09 18:29:28 +0900

    zzzz

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -6,9 +6,6 @@ use crate::serial_debug;
 use core::marker::FnPtr;
 
 use core::arch::asm;
-use elf::endian::LittleEndian;
-use elf::section::SectionHeader;
-use elf::ElfBytes;
 use hal_riscv::cpu::{Exception, Interrupt, Mie, Mip, Mstatus};
 
 #[inline(always)]
